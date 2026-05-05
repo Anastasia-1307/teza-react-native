75b81a6180a5975835d600baa8df378dde62f512
Anastasia-1307 <levodeanschi.anastasia@gmail.com>
2026-05-05 14:22:29 +0300

    update

--- utils/NetworkConfig.ts
+++ utils/NetworkConfig.ts
@@ -56,9 +56,7 @@ export class NetworkConfig {
     const POSSIBLE_IPS = [
       'http://localhost:8000',
       'http://127.0.0.1:8000',
-      'http://192.168.1.100:8000',
-      'http://10.0.2.2:8000',
-      'http://192.168.0.15:8000',
+
       
     ];
     
